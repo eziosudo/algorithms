198411529fee72e54ed7dbc2f29b3638d11dfcd7
eziosudo <eziosudo@gmail.com>
2022-07-09 20:30:43 +0800

    27. remove element 28. implement strStr() 35. search insert position

---
+++ leetcode/editor/en/[28]Implement strStr().py
@@ -0,0 +1,54 @@
+# Implement strStr(). 
+# 
+#  Given two strings needle and haystack, return the index of the first 
+# occurrence of needle in haystack, or -1 if needle is not part of haystack. 
+# 
+#  Clarification: 
+# 
+#  What should we return when needle is an empty string? This is a great 
+# question to ask during an interview. 
+# 
+#  For the purpose of this problem, we will return 0 when needle is an empty 
+# string. This is consistent to C's strstr() and Java's indexOf(). 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= haystack.length, needle.length <= 10⁴ 
+#  haystack and needle consist of only lowercase English characters. 
+#  
+#  Related Topics Two Pointers String String Matching 👍 4470 👎 3778
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0
+        for i in range(len(haystack)):
+            if i + len(needle) <= len(haystack) and haystack[i:i + len(needle)] == needle:
+                return i
+            i += 1
+        return -1
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+
+e = Solution()
+print(e.strStr("abcde", "cde"))
